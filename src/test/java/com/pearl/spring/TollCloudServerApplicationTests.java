d2dc155da5a01f1589110148e9b9f56aeed81bde
pe@rl <Muthubharathi.r@gmail.com>
2018-08-28 23:42:11 +0530

    Initia commit

---
+++ src/test/java/com/pearl/spring/TollCloudServerApplicationTests.java
@@ -0,0 +1,18 @@
+/*
+package com.pearl.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TollCloudServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+*/
